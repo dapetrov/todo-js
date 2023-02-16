95a06d549fa8b5464696d9a4d965b0f4536c20be
sublystrum <petrov082011@mail.ru>
2023-02-17 01:53:19 +0300

    added sript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const todo = {
   create(text) {
-    return ` <div class="list-todo_wrapper">
+    return `<div class="list-todo_wrapper">
     <ul class="list-todo">
       <li class="list-todo__li">
         <div class="list-todo__li__container">
@@ -8,8 +8,9 @@ const todo = {
             type="checkbox"
             class="list-todo__li__container__checkbox"
           /><label class="list-todo__li__container__label">${text}</label>
-          <button class="delete-todo" onclick="deleteToDo(event)">del
-        </div>
+          <a class="close-button" href="#" onclick="todo.delete(event)">
+            <span class="close-icon"></span>
+          </a>
       </li>
     </ul>
   </div>`;
@@ -24,19 +25,48 @@ const todo = {
       .insertAdjacentHTML("beforeend", this.create(elemText.value));
     elemText.value = "";
   },
+  delete(event) {
+    let target = event.target;
+    while (target != this) {
+      if (target.classList.contains("list-todo_wrapper")) {
+        target.remove();
+        return;
+      }
+      target = target.parentNode;
+    }
+    target.remove();
+  },
 };
 
-function AddNewToDo() {
-  todo.add();
-}
-function deleteToDo(event) {
-  let target = event.target;
-  while (target != this) {
-    if (target.classList.contains("list-todo_wrapper")) {
-      target.remove();
-      return;
+const todoALL = document.querySelector(".todo-all");
+todoALL.addEventListener("change", function (event) {
+  if (event.target.matches(".list-todo__li__container__checkbox")) {
+    const label = event.target.nextElementSibling;
+    if (event.target.checked) {
+      label.classList.add("crossed-out");
+    } else {
+      label.classList.remove("crossed-out");
     }
-    target = target.parentNode;
   }
-  target.remove();
-}
+});
+
+const allСheckbox = document.getElementById("all-checkboxes");
+const allСheckboxesLabel = document.querySelector(".label-for-checkbox");
+
+allСheckboxesLabel.addEventListener("click", function () {
+  allСheckbox.checked = !allСheckbox.checked;
+
+  const checkboxess = document.querySelectorAll(
+    ".list-todo__li__container__checkbox"
+  );
+
+  checkboxess.forEach((checkbox) => {
+    checkbox.checked = allСheckbox.checked;
+    const label = checkbox.nextElementSibling;
+    if (allСheckbox.checked) {
+      label.classList.add("crossed-out");
+    } else {
+      label.classList.remove("crossed-out");
+    }
+  });
+});
